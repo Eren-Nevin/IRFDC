f3f31261039ec34a7f6d0293273e1f7fce87b205
Eren Nevin <eren.m.nevin@gmail.com>
2023-08-01 20:30:33 +0300

    finished headers

--- src/routes/gather.ts
+++ src/routes/gather.ts
@@ -8,23 +8,28 @@ async function getCompaniesName(pageSize: number, pageNumber: number) {
     const res = await fetch(
         url, {
 
-            header
-
-            
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'en-US,en;q=0.9,fa-IR;q=0.8,fa;q=0.7,af;q=0.6',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Cookie': 'ASP.NET_SessionId=fwrevdckktgrf2b0vkspmsvn',
+            'DNT': '1',
+            'Pragma': 'no-cache',
+            'Referer': 'https://irc.fda.gov.ir/home/public/IRC',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"macOS"'
         }
+
+
+    }
 
 
     )
 }
 
-'Accept' : 'application/json, text/plain, */*'
-'Accept-Language' : 'en-US,en;q=0.9,fa-IR;q=0.8,fa;q=0.7,af;q=0.6'
-'Cache-Control' : 'no-cache'
-'Connection' : 'keep-alive'
-'Cookie' : 'ASP.NET_SessionId=fwrevdckktgrf2b0vkspmsvn'
-'DNT' : '1'
-'Pragma' : 'no-cache'
-'Referer' : 'https://irc.fda.gov.ir/home/public/IRC'
-'Sec-Fetch-Dest' : 'empty'
-'Sec-Fetch-Mode' : 'cors'
-'Sec-Fetch-Site' : 'same-origin'
